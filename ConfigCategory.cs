3d33f85fa444fdd5c51f86c26087d16e5dccb093
Moseskhan <34588306+Moseskhan@users.noreply.github.com>
2020-10-28 21:18:09 -0400

    first commit

---
+++ ConfigCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApiCallConsoleApp
+{
+	public class ConfigCategory
+	{
+		public string name { get; set; }
+		public int id { get; set; }
+		public int parentConfigCategoryId { get; set; }
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ApiCallConsoleApp
+{
+	class Program
+	{
+	
+
+		static async Task Main(string[] args)
+		{
+			Console.WriteLine("Kanja");
+			var httpCalls = new HttpCalls();
+			var addOns = await httpCalls.GetConfigurations();
+			Console.WriteLine(JsonConvert.SerializeObject(addOns));
+			
+		}
+
+		
+
+		
+	}
+
+	class HttpCalls
+	{
+		public async Task<List<ConfigCategory>> GetConfigurations()
+		{
+			using (var client = new HttpClient())
+			{
+				var url = "https://ecgcm-dev-eastus2.azurewebsites.net/api/config-category";
+				var content = await client.GetStringAsync(url);
+				return JsonConvert.DeserializeObject<List<ConfigCategory>>(content);
+			}
+		}
+	}
+}
